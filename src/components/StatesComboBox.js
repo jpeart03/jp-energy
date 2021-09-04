95c2c66b1b1bf94c8f30a65c0eef39d9a7cdc9d8
jpeart03 <james.r.peart@gmail.com>
2021-09-04 07:54:57 -0500

    update homepage content & fix combo box bugs

--- src/components/StatesComboBox.js
+++ src/components/StatesComboBox.js
@@ -1,104 +1,98 @@
 import { useState, useEffect } from "react";
 import { Autocomplete } from "@material-ui/lab";
 import {
-    TextField,
-    Button,
-    TableContainer,
-    Paper,
-    Table,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableBody,
+  TextField,
+  Button,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
 } from "@material-ui/core";
 import { getUSStateAbbr, getElecRetailPrice } from "../api/eia";
 
 import "./StatesComboBox.scss";
 
 export const StatesComboBox = () => {
-    const [statesList, setStatesList] = useState([]);
-    const [selectedState, setSelectedState] = useState({
-        name: "",
-        abbr: "",
-    });
-    const [elecRetailPrice, setElecRetailPrice] = useState([]);
+  const [statesList, setStatesList] = useState([]);
+  const [selectedState, setSelectedState] = useState({
+    name: "",
+    abbr: "",
+  });
+  const [elecRetailPrice, setElecRetailPrice] = useState([]);
 
-    useEffect(() => {
-        const fetchStatesAbbr = async () => {
-            const response = await getUSStateAbbr();
-            setStatesList(response.data);
-        };
-        fetchStatesAbbr();
-    }, []);
-
-    const handleSelect = (value) => {
-        setSelectedState(value);
+  useEffect(() => {
+    const fetchStatesAbbr = async () => {
+      const response = await getUSStateAbbr();
+      setStatesList(response.data);
     };
+    fetchStatesAbbr();
+  }, []);
 
-    const handleGetData = async () => {
-        const response = await getElecRetailPrice(selectedState.abbr);
-        if (response.data.data) {
-            console.log(response.data.data.error);
-        } else {
-            const data = response.data.series[0].data;
-            setElecRetailPrice(data);
-        }
-    };
+  const handleSelect = (value) => {
+    if (value) setSelectedState(value);
+  };
 
-    return (
-        <>
-            <Autocomplete
-                id="states-autocomplete"
-                className="auto-complete"
-                options={statesList}
-                getOptionLabel={(option) => option.name}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        label="Select a state"
-                        margin="normal"
-                        variant="outlined"
-                    />
-                )}
-                value={selectedState}
-                onChange={(_, value) => handleSelect(value)}
-                // override b/c using object as option
-                getOptionSelected={(option, value) =>
-                    option.abbr === value.abbr
-                }
-            />
-            <Button variant="contained" color="primary" onClick={handleGetData}>
-                Get energy data
-            </Button>
-            {elecRetailPrice.length > 0 && (
-                <TableContainer
-                    component={Paper}
-                    className="price-table-container"
-                >
-                    <Table aria-label="table of annual average state electricity prices">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Year</TableCell>
-                                <TableCell>Price $</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {elecRetailPrice.map((record) => (
-                                <TableRow
-                                    key={`${selectedState.abbr}-${record[0]}`}
-                                >
-                                    <TableCell component="th" scope="row">
-                                        {record[0]}
-                                    </TableCell>
-                                    <TableCell component="th" scope="row">
-                                        {record[1]}
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            )}
-        </>
-    );
+  const handleGetData = async () => {
+    const response = await getElecRetailPrice(selectedState.abbr);
+    if (response.data.data) {
+      setElecRetailPrice([]);
+      console.log(response.data.data.error);
+    } else {
+      const data = response.data.series[0].data;
+      setElecRetailPrice(data);
+    }
+  };
+
+  return (
+    <>
+      <Autocomplete
+        id="states-autocomplete"
+        className="auto-complete"
+        options={statesList}
+        getOptionLabel={(option) => option.name}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Select a state"
+            margin="normal"
+            variant="outlined"
+          />
+        )}
+        value={selectedState}
+        onChange={(_, value) => handleSelect(value)}
+        // override b/c using object as option
+        getOptionSelected={(option, value) => option.abbr === value.abbr}
+      />
+      <Button variant="contained" color="primary" onClick={handleGetData}>
+        Get energy data
+      </Button>
+      {elecRetailPrice.length > 0 && (
+        <TableContainer component={Paper} className="price-table-container">
+          <Table aria-label="table of annual average state electricity prices">
+            <TableHead>
+              <TableRow>
+                <TableCell>Year</TableCell>
+                <TableCell>Price $</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {elecRetailPrice.map((record) => (
+                <TableRow key={`${selectedState.abbr}-${record[0]}`}>
+                  <TableCell component="th" scope="row">
+                    {record[0]}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {record[1]}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </>
+  );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,21 +5,22 @@ import { StatesComboBox } from "../components/StatesComboBox";
 import "./Home.scss";
 
 const Home = () => {
-    return (
-        <Container className="home" maxWidth="md">
-            {/* Make this a hero component*/}
-            <Typography variant="h2" component="h1">
-                What's the source of your power?
-            </Typography>
-            <Typography variant="h6" component="p">
-                No, this isn't a super-hero site, but do you know what keeps the
-                lights on in your home state? How clean is your state's energy?
-                Lightsource can help you find out. Use the form below to get
-                started.
-            </Typography>
-            <StatesComboBox />
-        </Container>
-    );
+  return (
+    <Container className="home" maxWidth="md">
+      {/* Make this a hero component*/}
+      <Typography variant="h2" component="h1">
+        What's the source of your power?
+      </Typography>
+      <Typography variant="h6" component="p">
+        No - this isn't a super-hero site, but do you know what powers your home
+        state?
+      </Typography>
+      <Typography variant="h6" component="p">
+        Lightsource can help you find out. Use the form below to get started:
+      </Typography>
+      <StatesComboBox />
+    </Container>
+  );
 };
 
 export default Home;
