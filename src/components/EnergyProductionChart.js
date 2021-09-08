8b387062b4eccd720cf42c1147a3ad0b86a39861
jpeart03 <james.r.peart@gmail.com>
2021-09-07 19:11:07 -0500

    update EnergyProd... component to area chart

--- src/components/EnergyProductionChart.js
+++ src/components/EnergyProductionChart.js
@@ -1,91 +1,116 @@
 import { useState, useEffect } from "react";
 import { getEnergyProduction } from "../api/eia";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Typography, Link } from "@material-ui/core";
 import {
-    LineChart,
-    Line,
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    Tooltip,
-    ResponsiveContainer,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  AreaChart,
+  Area,
 } from "recharts";
 
 export const EnergyProductionChart = ({ stateAbbr, chartTitle }) => {
-    const [energyProduction, setEnergyProduction] = useState([]);
-    const [errorMessage, setErrorMessage] = useState([]);
+  const [energyProduction, setEnergyProduction] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    useEffect(() => {
-        const getChartData = async () => {
-            const production = await getEnergyProduction(stateAbbr);
-            if (production.error) {
-                setEnergyProduction([]);
-                setErrorMessage(production.error);
-            } else {
-                setEnergyProduction(production);
-                setErrorMessage("");
-            }
-        };
-        if (stateAbbr) {
-            getChartData();
-        }
-    }, [stateAbbr]);
-    // TODO CHANGE TO AREA CHART W/ LINEAR GRADIENT
-    return (
-        <>
-            {(energyProduction.length > 0 || errorMessage) && (
-                <Card className="chart-card">
-                    {errorMessage ? (
-                        <CardContent>
-                            <Typography>
-                                Sorry, we couldn't find any data for that state.
-                            </Typography>
-                        </CardContent>
-                    ) : (
-                        <CardContent>
-                            <Typography
-                                variant="h4"
-                                className="chart-card__title"
-                            >
-                                {chartTitle}
-                            </Typography>
-                            <ResponsiveContainer width="100%" height={300}>
-                                <LineChart
-                                    data={energyProduction}
-                                    margin={{
-                                        top: 10,
-                                        right: 30,
-                                        left: 30,
-                                        bottom: 5,
-                                    }}
-                                >
-                                    <CartesianGrid stroke="#606060" />
-                                    <XAxis
-                                        dataKey="year"
-                                        type="category"
-                                        tick={{ fill: "#fff" }}
-                                        dy={10}
-                                        interval={4}
-                                        reversed={true}
-                                    />
-                                    <YAxis tick={{ fill: "#fff" }} dx={-10} />
-                                    <Tooltip />
-                                    <Line
-                                        type="monotone"
-                                        dataKey="totalEnergyProd"
-                                        stroke="#2196f3"
-                                    />
-                                    <Line
-                                        type="monotone"
-                                        dataKey="renewableEnergyProd"
-                                        stroke="#4caf50"
-                                    />
-                                </LineChart>
-                            </ResponsiveContainer>
-                        </CardContent>
-                    )}
-                </Card>
-            )}
-        </>
-    );
+  useEffect(() => {
+    const getChartData = async () => {
+      const production = await getEnergyProduction(stateAbbr);
+      if (production.error) {
+        setEnergyProduction([]);
+        setErrorMessage(production.error);
+      } else {
+        setEnergyProduction(production);
+        setErrorMessage("");
+      }
+    };
+    if (stateAbbr) {
+      getChartData();
+    }
+  }, [stateAbbr]);
+  return (
+    <>
+      {(energyProduction.length > 0 || errorMessage) && (
+        <Card className="chart-card">
+          {errorMessage ? (
+            <CardContent>
+              <Typography>
+                Sorry, we couldn't find any data for that state.
+              </Typography>
+            </CardContent>
+          ) : (
+            <CardContent>
+              <Typography variant="h4" className="chart-card__title">
+                {chartTitle}
+              </Typography>
+              <ResponsiveContainer width="100%" height={300}>
+                <AreaChart
+                  data={energyProduction}
+                  margin={{
+                    top: 10,
+                    right: 30,
+                    left: 30,
+                    bottom: 5,
+                  }}
+                >
+                  <defs>
+                    <linearGradient id="colorTe" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#2196f3" stopOpacity={0.8} />
+                      <stop offset="95%" stopColor="#2196f3" stopOpacity={0} />
+                    </linearGradient>
+                    <linearGradient id="colorRe" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#4caf50" stopOpacity={0.8} />
+                      <stop offset="95%" stopColor="#4caf50" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                  <CartesianGrid stroke="#606060" />
+                  <XAxis
+                    dataKey="year"
+                    type="category"
+                    tick={{ fill: "#fff" }}
+                    dy={10}
+                    interval={4}
+                    reversed={true}
+                  />
+                  <YAxis tick={{ fill: "#fff" }} dx={-10} />
+                  <Tooltip
+                    formatter={(value, name, props) => [
+                      value,
+                      name === "totalEnergyProd" ? "Total" : "Renewable",
+                    ]}
+                  />
+                  <Area
+                    type="monotone"
+                    dataKey="totalEnergyProd"
+                    stroke="#2196f3"
+                    fillOpacity={1}
+                    fill="url(#colorTe)"
+                  />
+                  <Area
+                    type="monotone"
+                    dataKey="renewableEnergyProd"
+                    stroke="#4caf50"
+                    fillOpacity={1}
+                    fill="url(#colorRe)"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+
+              <Typography variant="caption">
+                <Link
+                  href="https://www.eia.gov/opendata/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Source: U.S. Energy Information Administration
+                </Link>
+              </Typography>
+            </CardContent>
+          )}
+        </Card>
+      )}
+    </>
+  );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,28 +7,28 @@ import { Charts } from "../components/Charts";
 import "./Home.scss";
 
 const Home = () => {
-    const [stateAbbr, setStateAbbr] = useState("");
-    const onGetEnergyData = (selectedStateAbbr) => {
-        setStateAbbr(selectedStateAbbr);
-    };
+  const [stateAbbr, setStateAbbr] = useState("");
+  const onGetEnergyData = (selectedStateAbbr) => {
+    setStateAbbr(selectedStateAbbr);
+  };
 
-    return (
-        <Container className="home" maxWidth="md">
-            <Typography variant="h2" component="h1">
-                What's the source of your power?
-            </Typography>
-            <Typography variant="h6" component="p">
-                No - this isn't a super-hero site, but do you know what powers
-                your home state? Lightsource can help you discover the sources
-                of your electricity and how much it costs!
-            </Typography>
-            <Typography variant="h6" component="p">
-                Use the form below to get started:
-            </Typography>
-            <StatesComboBox onGetEnergyData={onGetEnergyData} />
-            <Charts stateAbbr={stateAbbr} />
-        </Container>
-    );
+  return (
+    <Container className="home" maxWidth="md">
+      <Typography variant="h2" component="h1">
+        What's the source of your power?
+      </Typography>
+      <Typography variant="h6" component="p">
+        This isn't a super-hero site, but do you know what powers your home
+        state? Lightsource can help you discover the sources of your state's
+        electricity and how much it costs!
+      </Typography>
+      <Typography variant="h6" component="p">
+        Use the form below to get started:
+      </Typography>
+      <StatesComboBox onGetEnergyData={onGetEnergyData} />
+      <Charts stateAbbr={stateAbbr} />
+    </Container>
+  );
 };
 
 export default Home;
