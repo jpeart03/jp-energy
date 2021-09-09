b723cb3af2d5e8ee368798024b60963621f07b14
jpeart03 <james.r.peart@gmail.com>
2021-09-09 09:04:41 -0500

    add error prevention to logout menu item. add NotFound component for bad routes

---
+++ src/components/LogoutDialog.js
@@ -0,0 +1,46 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@material-ui/core";
+
+const LogoutDialog = ({ open, onClose, onLogout }) => {
+    const handleClose = () => {
+        onClose();
+    };
+
+    const handleLogout = () => {
+        onLogout();
+    };
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                {"Are you sure you want to logout?"}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    You must be logged in to use the "Compare States" feature.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={handleLogout} color="primary" autoFocus>
+                    Logout
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default LogoutDialog;
