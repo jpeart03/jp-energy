b2a0b26d61c3f5de6d58fe74301b4c2c6404ae3c
jpeart03 <james.r.peart@gmail.com>
2021-09-08 21:11:49 -0500

    add start of state compare page and necessary components

---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div style={{ margin: "7rem" }}>NOT FOUND</div>;
+};
+
+export default NotFound;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 import Home from "../pages/Home";
+import Compare from "../pages/Compare";
 import AppNav from "./AppNav";
+import NotFound from "./NotFound";
 
 const Routes = () => {
   return (
     <Router>
       <AppNav />
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/compare" component={Compare} />
+        {/* NotFound for invalid routes */}
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
